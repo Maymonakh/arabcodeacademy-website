447dd39fc5d1ff6da747740cec2f13ff28aaafdb
Maymonakh <maymonakhanfar@gmail.com>
2024-10-21 22:24:06 +0300

    Create ads panel

---
+++ app/components/AdsPanel.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+import styles from "../styles/AdPanel.module.css";
+
+interface AdPanelProps {
+  startDate: string;
+  endDate: string;
+  adText: string;
+}
+
+const AdPanel: React.FC<AdPanelProps> = ({ startDate, endDate, adText }) => {
+  const now = new Date();
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const isAdActive = now >= start && now <= end;
+
+  return (
+    <>
+      {isAdActive && (
+        <Box className={styles.adPanel}>
+          <Text >{adText}</Text>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default AdPanel;
