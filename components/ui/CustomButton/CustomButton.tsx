6c5e06bd7ca0f9aaf263093be1dad656cc1ec0e6
Maymonakh <maymonakhanfar@gmail.com>
2024-11-04 23:06:10 +0200

    Update responsive panel and button

--- components/ui/CustomButton/CustomButton.tsx
+++ components/ui/CustomButton/CustomButton.tsx
@@ -4,7 +4,7 @@ import { ReactElement } from "react";
 interface CustomButtonProps {
   text: string;
   icon?: ReactElement;
-  buttonType?: "primary" | "secondary";
+  buttonType?: "primary" | "secondaryOne" | "secondaryTwo";
   color?: "green" | "orange";
   onClick?: () => void;
 }
@@ -16,21 +16,25 @@ const CustomButton: React.FC<CustomButtonProps> = ({
   color = "green",
   onClick,
 }) => {
-
   const [isMobile] = useMediaQuery("(max-width: 480px)");
-  const [isTablet] = useMediaQuery("(min-width: 481px) and (max-width: 768px)");
+  const [isTablet] = useMediaQuery("(min-width: 481px) and (max-width: 850px)");
 
   const buttonStyles = {
     primary: isMobile
       ? { height: "60px", width: "200px", fontSize: "17px" }
       : isTablet
       ? { height: "85px", width: "330px", fontSize: "17px" }
-      : { height: "80px", width: "310px", fontSize: "17px" }, 
-    secondary: isMobile
+      : { height: "80px", width: "310px", fontSize: "17px" },
+    secondaryOne: isMobile
       ? { height: "50px", width: "150px", fontSize: "17px" }
       : isTablet
       ? { height: "70px", width: "234px", fontSize: "17px" }
       : { height: "60px", width: "200px", fontSize: "17px" },
+    secondaryTwo: isMobile
+      ? { height: "40px", width: "100px", fontSize: "17px" }
+      : isTablet
+      ? { height: "50px", width: "160px", fontSize: "17px" }
+      : { height: "44px", width: "140px", fontSize: "17px" },
   };
 
   const colorStyles = {
