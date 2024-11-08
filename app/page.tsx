e3d9083920641bfb93cf5af4f33ea7a62df7ae24
tharaasukaina <tharaasukaina@gmail.com>
2024-11-08 16:38:42 +0200

    إضافة التعديلات إلى task/tharaa

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/variables.css";
 import ProgrammingTests from "../sections/home/ProgrammingTests/ProgrammingTests";
 import InteractiveTools from "@/sections/home/InteractiveTools/InteractiveTools";
+import EduPath from "../sections/home/EduPath/EduPath";
 
 export default function Home() {
   return (
     <ChakraProvider>
-      <AdsPanel
+      {/* <AdsPanel
         startDate="2024-10-20T00:00:00Z"
         endDate="2024-11-31T23:59:59Z"
         adText="خصومات بنسبة 20% على الكورسات"
       />
+
       <ProgrammingTests />
       <InteractiveTools />
+=======
+      <ProgrammingTests /> */}
+
+      {/* <EduPath /> */}
     </ChakraProvider>
   );
 }
