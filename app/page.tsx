54aafd7880fb84b2a73fcfa08e86775700b0b67f
tharaasukaina <tharaasukaina@gmail.com>
2024-11-14 13:37:23 +0200

    befor slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,49 @@
-import AdsPanel from "../sections/home/AdsPanel/AdsPanel";
- import { ChakraProvider } from "@chakra-ui/react";
-import "../styles/variables.css";
-import ProgrammingTests from "../sections/home/ProgrammingTests/ProgrammingTests";
-import EduPath from '../sections/home/EduPath/EduPath';
-import PromoText from '../components/ui/PromoText/PromoText';
+
+import { ChakraProvider, SimpleGrid } from "@chakra-ui/react";
+import SimpleCard from '../components/ui/Card/simpleCard';
+import img1 from '@/public/images/kisspng-portrait-shannon-5ae540d7d5c1d0.2959227015249737838756 1.svg';
+import img2 from '@/public/images/2.svg';
+import img3 from '@/public/images/3.svg';
+
+
+const cardsData = [
+  { name: "اسم المستخدم",
+     paragraph: "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية وأسلوب تدريسي ممتع يتناسب",
+     imageSrc:img1,
+     date:'2023, 11 نيسان',
+     rating: 4
+     },
+  { name: "اسم المستخدم",
+    paragraph: "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية وأسلوب تدريسي ممتع يتناسب",
+    imageSrc: img2 ,
+    date:'2023, 11 نيسان',
+    rating: 4
+     },
+  { name: "اسم المستخدم", 
+    paragraph: "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية وأسلوب تدريسي ممتع يتناسب.", 
+    imageSrc: img3, 
+    date: '2023, 11 نيسان'  ,
+    rating: 4 },
+ 
+
+
+];
+
 export default function Home() {
   return (
     <ChakraProvider>
-      {/* <AdsPanel
-        startDate="2024-10-20T00:00:00Z"
-        endDate="2024-11-31T23:59:59Z"
-        adText="خصومات بنسبة 20% على الكورسات"
-      />
-      <ProgrammingTests /> */}
-      {/* <ProgrammingTests/> */}
-     <EduPath/>
-     
+      <SimpleGrid columns={[1, 2, 3]} spacing={4} p={4}>
+        {cardsData.map((card, index) => (
+          <SimpleCard
+            key={index}
+            name={card.name}
+            paragraph={card.paragraph}
+            imageSrc={card.imageSrc}
+            date={card.date}
+            rating={card.rating}
+          />
+        ))}
+      </SimpleGrid>
     </ChakraProvider>
   );
 }
