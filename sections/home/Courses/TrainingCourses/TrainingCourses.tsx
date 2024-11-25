e43d5c7fa88cb7627d2a60e526b241a0919b0400
Misk <misk.s.ashoor@gmail.com>
2024-11-25 14:21:24 +0200

    misk updates

--- sections/home/Courses/TrainingCourses/TrainingCourses.tsx
+++ sections/home/Courses/TrainingCourses/TrainingCourses.tsx
@@ -62,14 +62,6 @@ const TrainingCourses = () => {
     "(min-width: 900px) and (max-width: 1441px)",
   ]);
 
-  const positionValue = isMobile
-    ? "-20%"
-    : isTablet1
-    ? "-17%"
-    : isTablet2
-    ? "-35%"
-    : "-13%";
-
   return (
     <div className={styles.CardsContainer}>
       <CustomSwiper
@@ -96,12 +88,16 @@ const TrainingCourses = () => {
       />
       <ArrowButton
         direction="left"
-        positionValue={positionValue}
+        positionValue={
+          isMobile ? "-0%" : isTablet1 ? "-17%" : isTablet2 ? "-35%" : "-13%"
+        }
         onClick={handlePrev}
       />
       <ArrowButton
         direction="right"
-        positionValue={positionValue}
+        positionValue={
+          isMobile ? "-8%" : isTablet1 ? "-17%" : isTablet2 ? "-35%" : "-13%"
+        }
         onClick={handleNext}
       />
     </div>
