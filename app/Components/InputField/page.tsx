f72d32a892376e349d6c286219e245ec67f20fd2
Nada Obaid <nada.s.obaidd@gmail.com>
2025-01-11 23:38:58 +0200

    add SignIn page

---
+++ app/Components/InputField/page.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Image from 'next/image';
+import './InputField.css';
+
+interface InputFieldProps {
+    id: string;
+    label: string;
+    placeholder: string;
+    iconSrc: string;
+    showEyeIcon?: boolean;
+    errorMessage?: string;
+    onFocus?: () => void;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  }
+  
+  const InputField: React.FC<InputFieldProps> = ({
+    id,
+    label,
+    placeholder,
+    iconSrc,
+    showEyeIcon = false,
+    errorMessage,
+    onFocus,
+    value,
+    onChange
+  }) => {
+
+    const labelClass = errorMessage ? "label-with-icon error" : "label-with-icon"; 
+  
+    return (
+      <div className="form-group">
+        <label htmlFor={id} className={labelClass}>
+          <Image src={iconSrc} alt="icon" width={15} height={15} />
+          {label}
+        </label>
+        <div className="input-container">
+          <input
+            type="text"
+            id={id}
+            placeholder={placeholder}
+            onFocus={onFocus}
+            value={value}
+            onChange={onChange}
+            className={`input-container ${errorMessage ? "error" : ""}`}
+          />
+          {showEyeIcon && (
+            <span className="input-icon">
+              <Image 
+                src="/icons/eye-slash.svg" 
+                alt="eye-icon" 
+                width={15} 
+                height={15} 
+              />
+            </span>
+          )}
+        </div>
+        {errorMessage && <span className="error-message">{errorMessage}</span>}
+      </div>
+    );
+  };
+
+
+export default InputField;
---
+++ app/Components/SocialButton/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./SocialButton.css";
+import separatorImage from '../../../public/icons/Line 10.svg'
+import Image from "next/image";
+
+interface SocialButtonProps {
+  iconClass: string;
+  buttonClass: string;
+  text: string;
+}
+
+const SocialButton: React.FC<SocialButtonProps> = ({ iconClass, buttonClass, text }) => {
+  return (
+    <button className={`social-button ${buttonClass}`}>
+      <i className={iconClass}></i>
+      <Image src={separatorImage} alt="Separator" />  
+      <span>{text}</span>
+    </button>
+  );
+};
+
+export default SocialButton;
